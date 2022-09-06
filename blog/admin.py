c5b3ade3f2c6e52ff2e6c33d5f7f17e6f7643d35
Povladislav <vladpomalejko@gmail.com>
2022-09-06 15:29:27 +0300

    litle changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post, Page, Tag
 
-admin.register(Page, Post, Tag)
-# Register your models here.
+admin.site.register(Page)
+admin.site.register(Post)
+admin.site.register(Tag)
