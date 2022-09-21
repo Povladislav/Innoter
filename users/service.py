c83b024a40b4813e23b731d9b15322af2bc15ac6
Povladislav <vladpomalejko@gmail.com>
2022-09-21 15:56:32 +0300

    Final adjust temporal ban users

--- users/service.py
+++ users/service.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from .models import User
 from .permissions import IsUserAdm
 
+from django.utils import timezone
+
 
 class AdminLogic(GenericAPIView):
     permission_classes = [IsUserAdm]
@@ -20,6 +22,6 @@ class AdminLogic(GenericAPIView):
             return Response({"banned": "successfully permanently banned"})
         user_to_ban = User.objects.get(pk=id)
         user_to_ban.is_blocked = True
-        user_to_ban.time_before_unban += datetime.timedelta(minutes=time)
+        user_to_ban.time_before_unban = timezone.now() + datetime.timedelta(minutes=time)
         user_to_ban.save()
         return Response({"banned": f"successfully banned for {time} minutes"})
